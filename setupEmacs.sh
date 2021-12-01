f0bb08d99fde56c18cc8996f123e64a78e44ede6
cmahk <cmahk001@ucr.edu>
2021-11-30 19:19:54 -0800

    Removed old emacs folder

--- setupEmacs.sh
+++ setupEmacs.sh
@@ -2,8 +2,12 @@
 
 git clone https://git.savannah.gnu.org/git/emacs.git
 cd ./emacs
-git checkout emacs-27.2
+git checkout emacs-24.3
 
 source ./autogen.sh
-./configure --without-selinux
-make -j4
+cd ..
+tar --create --file emacs-24.3.tar.gz ./emacs-24.3/
+mv emacs-24.3.tar.gz ./rpmbuild/SOURCES/
+
+cd ./rpmbuild/
+rpmbuild --define "_topdir `pwd`" -v -bb SPECS/emacs.spec
